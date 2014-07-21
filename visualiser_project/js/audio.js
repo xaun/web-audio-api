4a7ba868594773a6434544d44fa7264b4bb489aa
Xaun Lopez <xaunlopez@gmail.com>
2014-07-21 21:42:15 +1000

    Audio object with phaser effect working!!!

--- visualiser_project/js/audio.js
+++ visualiser_project/js/audio.js
@@ -14,27 +14,31 @@ var Audio = {
       alert('Web Audio API is not supported in this browser');
     }
   },
-  audiourl: "assets/VersionsModerat.mp3",
+  audioUrl: "assets/VersionsModerat.mp3",
   audioData: null,
   audioPlaying: false,
   sampleSize: 1024,
   setupPhaserNodes: function () {
+    // Monkey patches for Tuna
+    Audio.audioContext.createGainNode = Audio.audioContext.createGain;
+    Audio.audioContext.createDelayNode = Audio.audioContext.createDelay;
+    Audio.audioContext.createJavaScriptNode = Audio.audioContext.createScriptProcessor;
     Audio.tuna = new Tuna(Audio.audioContext);
-    Audio.phaser = new tuna.Phaser({
+    Audio.phaser = new Audio.tuna.Phaser({
       rate: 1.2,                     //0.01 to 8 is a decent range, but higher values are possible
       depth: 0.3,                    //0 to 1
       feedback: 0.2,                 //0 to 1+
       stereoPhase: 30,               //0 to 180
       baseModulationFrequency: 700,  //500 to 1500
       bypass: 0
-    };
+    });
     Audio.sourceNode = Audio.audioContext.createBufferSource();
     Audio.analyserNode = Audio.audioContext.createAnalyser();
   },
   connectPhaserNodes: function () {
     Audio.sourceNode.connect(Audio.phaser.input);
-    Audio.phaser.connect(audioContext.destination);
-    Audio.phaser.connect(analyserNode);
+    Audio.phaser.connect(Audio.audioContext.destination);
+    Audio.phaser.connect(Audio.analyserNode);
   },
   getFrequencyDomain: function () {
     var frequencyData = new Uint8Array(Audio.analyserNode.frequencyBinCount);
@@ -52,10 +56,12 @@ var Audio = {
     request.responseType = 'arraybuffer';
 
     request.onload = function () {
-      Audio.AudioContext.decodeAudioData(request.response, function (buffer) {
-        Audio.audioData = buffer;
-        playSound(audioData);
-      }, onError);
+      // debugger;
+      Audio.audioContext.decodeAudioData(request.response,
+        function (buffer) {
+          Audio.audioData = buffer;
+          Audio.playSound(Audio.audioData);
+      }, null);
     }
     request.send();
   },
@@ -64,17 +70,17 @@ var Audio = {
     Audio.sourceNode.start(0);
     Audio.sourceNode.loop = true;
     Audio.audioPlaying = true;
-  },
-  onError: function (e) {
-    console.log(e);
   }
 };
 
 $(document).ready(function () {
+  Audio.audioContextSetup();
+
 
   $("#start").on('click', function (e) {
     e.preventDefault();
     console.log('playing')
+
 
     Audio.setupPhaserNodes();
     Audio.connectPhaserNodes();
